ba00ce83e3457c64b793862a5a94806f4a500555
Mael Trean <Mael Trean>
2016-12-05 11:16:56 +0100

    Push detrend/devar/detout

--- utils.py
+++ utils.py
@@ -7,6 +7,19 @@ import matplotlib
 matplotlib.style.use('ggplot')
 from datetime import datetime, date
 
+empty_weeks = [(datetime(year=2012,month=12,day=28),datetime(year=2013,month=1,day=4)),
+                   (datetime(year=2013,month=2,day=2),datetime(year=2013,month=2,day=9)),
+                   (datetime(year=2013,month=3,day=6),datetime(year=2013,month=3,day=13)),
+                   (datetime(year=2013,month=4,day=10),datetime(year=2013,month=4,day=17)),
+                   (datetime(year=2013,month=5,day=13),datetime(year=2013,month=5,day=20)),
+                   (datetime(year=2013,month=6,day=12),datetime(year=2013,month=6,day=19)),
+                   (datetime(year=2013,month=7,day=16),datetime(year=2013,month=7,day=23)),
+                   (datetime(year=2013,month=8,day=15),datetime(year=2013,month=8,day=22)),
+                   (datetime(year=2013,month=9,day=14),datetime(year=2013,month=9,day=21)),
+                   (datetime(year=2013,month=10,day=18),datetime(year=2013,month=10,day=25)),
+                   (datetime(year=2013,month=11,day=20),datetime(year=2013,month=11,day=27)),
+                   (datetime(year=2013,month=12,day=22),datetime(year=2013,month=12,day=29))]
+
 def approx_svd(mat, k):
     U,s,V = np.linalg.svd(mat, full_matrices=False)
     S = np.diag(s[0:k])
@@ -127,12 +140,7 @@ def create_week_matrix(df, weekday, day_max = -1):
     while True:
         if (first_day+pd.Timedelta('6 day')).date()>=last_day or (day_max != -1 and (first_day+pd.Timedelta('6 day')).date()>=day_max):
             break
-        arr = np.array(df.loc[first_day:first_day+pd.Timedelta('7 day')][:-1].fillna(method="bfill").fillna(method="ffill").transpose().values)
-        if np.sum(np.isnan(arr)) >0:
-            arr = np.zeros(shape=arr.shape)
-        for (d1,d2) in empty_weeks:
-            if (first_day>=d1 and first_day<d2) or (first_day+pd.Timedelta('6 day')>=d1 and first_day+pd.Timedelta('6 day')<d2):
-                arr = np.nan*np.zeros(shape=arr.shape)
+        arr = np.array(df.loc[first_day:first_day+pd.Timedelta('7 day')][:-1].transpose().values)
 
         if len(result) == 0:
             result = arr
